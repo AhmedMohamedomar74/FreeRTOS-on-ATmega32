16d63347f7ca75ab2b23d1ff0fe8e0f3c70471be
Ahmed Mohamed Omar <119351564+AhmedMohamedomar74@users.noreply.github.com>
2024-03-19 00:12:17 +0200

    Update main.c

--- Handle_with_Interrupt/APPLICATION/main.c
+++ Handle_with_Interrupt/APPLICATION/main.c
@@ -4,91 +4,47 @@
 #include "../RTOS/semphr.h"
 #include "../MCAL/Interrupt/Interrupt_EXT.h"
 
-/* Define Tasks Priorities */
-#define TASK2_PRIORITY (2)
-#define TASK1_PRIORITY (2)
-
-#define TASK1_STACK_SIZE (configMINIMAL_STACK_SIZE * 2)
-#define TASK2_STACK_SIZE (configMINIMAL_STACK_SIZE * 2)
+#define TASK_ISR_PRIORITY (2)
 #define TASK_ISR_STACK_SIZE (configMINIMAL_STACK_SIZE * 2)
 
-/*tasks codes prototypes */
-void task1_code(void *pvParamter);
-void task2_code(void *pvParamter);
 void task_ISR(void *pvParamter);
 void Task_Realse();
-xSemaphoreHandle LCD_SEM;
+
 xSemaphoreHandle task_sync_sem;
+
 int main(void)
 {
+    task_sync_sem = xSemaphoreCreateCounting(1, 0);
+    init_lcd();
 
-	vSemaphoreCreateBinary(LCD_SEM);
-	task_sync_sem = xSemaphoreCreateCounting(1,0);
-	init_lcd();
-	Interrupt_EXT_t INT_OBJ = {
-		.Call_back_fun = Task_Realse,
-		.INT_CONTROL = falling_edge,
-		.INT_NUM = EXT_INT_Number_INT0,
-	};
-	Test_Enable_init(&INT_OBJ);
-	/*Creat tasks*/
-	// xTaskCreate(task2_code, NULL, TASK2_STACK_SIZE, NULL, TASK2_PRIORITY, NULL);
-	// xTaskCreate(task1_code, NULL, TASK1_STACK_SIZE, NULL, TASK1_PRIORITY, NULL);
-	xTaskCreate(task_ISR, NULL, TASK_ISR_STACK_SIZE, NULL, TASK1_PRIORITY, NULL);
+    Interrupt_EXT_t INT_OBJ = {
+        .Call_back_fun = Task_Realse,
+        .INT_CONTROL = falling_edge,
+        .INT_NUM = EXT_INT_Number_INT0,
+    };
+    Test_Enable_init(&INT_OBJ);
 
-	/*start Scheduler */
-	vTaskStartScheduler();
-	
-	return 0;
-}
+    xTaskCreate(task_ISR, NULL, TASK_ISR_STACK_SIZE, NULL, TASK_ISR_PRIORITY, NULL);
 
-/*Task1 Code */
-void task1_code(void *pvParamter)
-{
-	for (;;)
-	{
-		if (xSemaphoreTake(LCD_SEM,portMAX_DELAY) == 1)
-		{
-			LCD_clear();
-			LCD_vidWriteString("I am Task 1");
-			vTaskDelay(2000); // Running to waiting for time 2000 Tick
-			
-		}
-		xSemaphoreGive(LCD_SEM);
-	}
-}
-/*Task2 Code */
-void task2_code(void *pvParamter)
-{
-	for (;;)
-	{
-		if (xSemaphoreTake(LCD_SEM,portMAX_DELAY) == 1)
-		{
-			LCD_clear();
-			LCD_vidWriteString("I am Task 2");
-			vTaskDelay(1000);	
-			
-		}
-		xSemaphoreGive(LCD_SEM);
-		
-	}
+    vTaskStartScheduler();
+
+    return 0;
 }
 
 void task_ISR(void *pvParamter)
 {
-	while (1)
-	{
-		if (xSemaphoreTake(task_sync_sem, portMAX_DELAY) == pdTRUE)
-		{
-			LCD_vidWriteString("Button Pressed:D");
-			_delay_ms(500);
-			LCD_clear();
-		}
-	}
-	
+    while (1)
+    {
+        if (xSemaphoreTake(task_sync_sem, portMAX_DELAY) == pdTRUE)
+        {
+            LCD_vidWriteString("Button Pressed:D");
+            _delay_ms(500);
+            LCD_clear();
+        }
+    }
 }
 
 void Task_Realse()
 {
-	xSemaphoreGiveFromISR(task_sync_sem,NULL);
+    xSemaphoreGiveFromISR(task_sync_sem, NULL);
 }
---
+++ Handle_with_Interrupt/README.md
@@ -0,0 +1,30 @@
+# FreeRTOS on ATmega32
+
+🔧 This project demonstrates the usage of FreeRTOS on ATmega32 microcontroller to manage two tasks concurrently.
+
+## Description
+
+📝 The project initializes two tasks, `Task1` and `Task2`, which execute concurrently with different priorities. FreeRTOS scheduler handles these tasks efficiently.
+
+## Task Priorities
+
+- `Task1`: Priority 2
+- `Task2`: Priority 1
+
+## Task Codes
+
+- `task1_code`: Displays "I am Task 1" on the LCD screen every 2 seconds.
+- `task2_code`: Displays "I am Task 2" on the LCD screen every 1 second.
+
+## Task Handling
+
+🔄 FreeRTOS handles the execution of tasks through its task scheduler. Tasks are created using the `xTaskCreate()` function, specifying their respective entry functions (`task1_code` and `task2_code`), stack size, priority, and other parameters.
+
+Once tasks are created, the scheduler decides which task to run based on their priorities. Higher priority tasks preempt lower priority tasks, ensuring that critical tasks are executed promptly.
+
+Tasks can be suspended or delayed using FreeRTOS API functions like `vTaskDelay()`, allowing for precise timing control. When a task is delayed, the scheduler switches to another task until the delay expires.
+
+Overall, FreeRTOS provides a flexible and efficient mechanism for multitasking on microcontrollers like ATmega32, enabling developers to build complex embedded systems with ease.
+
+## Demo GIF
+![VID_20240318_141622-ezgif com-video-to-gif-converter](https://github.com/AhmedMohamedomar74/FreeRTOS-on-ATmega32/assets/119351564/ac27c002-0fe6-4121-af4a-d577e15e5be6)
